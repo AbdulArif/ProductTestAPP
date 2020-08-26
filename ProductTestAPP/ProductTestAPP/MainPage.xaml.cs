d86bf7b6b9bddcbfdbe69f183bf8c925be4e1c62
AbdulArif <abdularif.dgp@gmail.com>
2020-08-26 21:17:18 +0530

    Add project files.

---
+++ ProductTestAPP/ProductTestAPP/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using ProductTestAPP.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ProductTestAPP
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            GetProducts();
+        }
+
+        private async void GetProducts()
+        {
+            HttpClient client = new HttpClient();
+            //var response = await client.GetStringAsync("https://localhost:44342/Product/GetProducts");
+            // var response = await client.GetStringAsync("http://localhost:56910/Product/GetProducts");
+            //var response = await client.GetStringAsync("http://45.64.225.33:56910/Product/GetProducts");
+            //var response = await client.GetStringAsync("http://192.168.0.101:56910/Product/GetProducts");
+            var response = await client.GetStringAsync("http://192.168.43.196:45457/Product/GetProducts");
+        
+            var products = JsonConvert.DeserializeObject<List<Products>>(response);
+            ProductListView.ItemsSource = products;
+        }
+    }
+}
---
+++ ProductTestAPP/ProductTestAPP/Models/Products.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductTestAPP.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Price { get; set; }
+    }
+    //public class Root
+    //{
+    //    public List<Products> Product { get; set; }
+    //}
+}
